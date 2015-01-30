6e4c52b9f8c53fcd6865a8420fe2d061d3351984
Minh Duc Ngo <wunsch85@gmail.com>
2015-01-30 23:14:45 +0100

    HelloWorld: create the first project

---
+++ HelloWorld/src/com/svcntt/tuto/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.svcntt.tuto.java;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World! Hope you enjoy this tutorial.");
+		
+		System.out.println("The first tutorial ends here!");
+	}
+}
